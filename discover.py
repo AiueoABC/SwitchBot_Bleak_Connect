f66161c88b79442697e37be4f001c7f8baf44e07
Ueo ABC, Ai <53726452+AiueoABC@users.noreply.github.com>
2022-05-31 16:15:06 +0900

    comment in English

--- discover.py
+++ discover.py
@@ -1,16 +1,27 @@
 """
-周辺にあるbluetooth機器を探索し、macアドレス、名前を表示するプログラム
+Scan blutooth devices and print them
 """
-
 import asyncio
-from bleak import discover
+from bleak import BleakScanner
+devices = {}
 
-async def run():
-    devices = await discover()
-    for d in devices:
-        #見つかった機器のmacアドレス、名前、距離を表示
-        print(d.address,d.name,d.rssi)
-        print(d)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run())
+def detection_callback(device, advertisement_data):
+    print(device.address, "RSSI:", device.rssi, advertisement_data)
+    if b"`U\xf9+]\xc6\x01\x00\x00'\x00\x00" in tuple(advertisement_data.manufacturer_data.values()):
+        # I guess manufacturer data should always be same?
+        devices[device.address] = advertisement_data
+
+
+async def main():
+    scanner = BleakScanner()
+    scanner.register_detection_callback(detection_callback)
+    await scanner.start()
+    await asyncio.sleep(5.0)
+    await scanner.stop()
+
+    # for d in scanner.discovered_devices:
+    #     print(d)
+    print(devices)
+
+asyncio.run(main())
